d68770a10773607d5b4bf939f0388f3681c80e2e
iurii stepin <y.stepin@x5.ru>
2024-10-28 00:17:09 +0300

    chore: Update dependencies and refactor index.ts for dotenv integration

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,4 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express();
-const PORT = 3000;
-
-app.get('/events', (req, res) => {
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('Connection', 'keep-alive');
-
-  const sendEvent = (data: string) => {
-    res.write(`data: ${data}\n\n`);
-  };
-
-  sendEvent('Connected to server');
-
-  const intervalId = setInterval(() => {
-    sendEvent(`Current time: ${new Date().toLocaleTimeString()}`);
-  }, 1000);
-
-  req.on('close', () => {
-    clearInterval(intervalId);
-    res.end();
-  });
-});
-
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
-export default app; // Экспортируйте приложение
